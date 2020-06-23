149c95e0ca978e2090df4612259866708df78b85
AndySocer <61567132+AndySocer@users.noreply.github.com>
2020-06-22 22:25:27 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,52 @@
+// Initialize your app
+var app = new Framework7({
+	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: false
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'Pizza Time',
+	dialog: {
+		title: 'Pizza Time',
+		buttonOk: 'Aceptar',
+  	},
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+        {
+		path: '/pizza/',
+    	url: 'pizza.html',
+    	name: 'pizza',
+  		},
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+$$('#btnAlerta').on('click', function(){
+    app.dialog.alert('Hola Diseño Web 2');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
